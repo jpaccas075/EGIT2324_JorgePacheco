1a4b24c5b79fb99c3303d2162633aa9d0f4b3996
Jorge Pacheco <a_jorge.pacheco.castro@iespablopicasso.es>
2023-11-29 09:38:03 +0100

    Primer commit

---
+++ ExGit2324/src/Persona.java
@@ -0,0 +1,13 @@
+
+public class Persona {
+	public String nombre = "";
+	public String apellidos = "";
+	public int edad;
+	
+	public Persona(String nombre, String apellidos, int edad) {
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.edad = edad;
+	}
+
+}
